9a8e3134582ed98137203ac75e92b1bf14f092f4
nspbdz <nspbdz@gmail.com>
2024-01-30 14:00:26 +0700

    fix(product):detail

--- app/Http/Controllers/Backend/HomeCtr.php
+++ app/Http/Controllers/Backend/HomeCtr.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductVariant;
 use App\Models\PurchaseCart;
+use App\Models\VariantAttribute;
 use Illuminate\Http\Request;
 
 class HomeCtr extends Controller
@@ -27,25 +28,24 @@ class HomeCtr extends Controller
     public function detail($id)
     {
         $product_details =Product::with('category')->where('id', $id)->first();
-        $promoPrice = $product_details['promo_price'];
+        $promoPrice = $product_details['promo_price'] ?? null;
 
         // Memeriksa kondisi dan menetapkan nilai promo_price menjadi NULL jika kondisi tidak terpenuhi
-        if ($product_details['is_promo'] == 1 && $product_details['promo_start_date'] <= date('Y-m-d') && $product_details['promo_end_date'] >= date('Y-m-d')) {
+        if ($product_details && $product_details['is_promo'] == 1 && $product_details['promo_start_date'] <= date('Y-m-d') && $product_details['promo_end_date'] >= date('Y-m-d')) {
             // Kondisi promo berlaku, promo_price tetap seperti sebelumnya
-
+        
         } else {
             // Kondisi promo tidak berlaku, set promo_price menjadi NULL
             $promoPrice = null;
         }
 
-        $variants = ProductVariant::select('id','code','name')->where('attribute_id',  1)->get();
-        $colors = ProductVariant::select('id','code','name')->where('attribute_id',  2)->get();
+        $variants=VariantAttribute::with('attributeValue')->get();
+
         return response()->json([
             
             "promoPrice" => $promoPrice,
             "product_details" => $product_details,
             "variants" => $variants,
-            "colors" => $colors,
         ], 200);
     }
 
