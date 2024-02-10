7211d505be1a3d60c705c1623844b3cd3b1bbbdb
nspbdz <nspbdz@gmail.com>
2024-02-10 20:19:25 +0700

    fix(homectr):deftail

--- app/Http/Controllers/Backend/HomeCtr.php
+++ app/Http/Controllers/Backend/HomeCtr.php
@@ -9,47 +9,50 @@ use App\Models\ProductVariant;
 use App\Models\PurchaseCart;
 use App\Models\VariantAttribute;
 use Illuminate\Http\Request;
+use DB;
 
 class HomeCtr extends Controller
 {
     public function index()
     {
-            $categoryProducts= Category::select('id','name')->get();
+        $categoryProducts = Category::select('id', 'name')->get();
 
-            $products =Product::select('id','name','price','image','category_id')->with('category')->get();
+        $products = Product::select('id', 'name', 'price', 'image', 'category_id')->with('category')->get();
 
-            return response()->json([
-                "products" => $products,
-                "categoryProducts" => $categoryProducts,
-            ], 200);
+        return response()->json([
+            "products" => $products,
+            "categoryProducts" => $categoryProducts,
+        ], 200);
     }
 
 
     public function detail($id)
     {
-        // dari variants relasi ke variant option lalu variant options relasi ke variant attributes ke variant attributes values 
-        $product_details =Product::with('category', 'variants','variants.variant_option.variant_atrributes.variant_attributes_values','variants.variant_option','variants.variant_option.variant_atrributes')->where('id', $id)->first();
-        $promoPrice = $product_details['promo_price'] ?? null;
+        $product_details = DB::table('products')
+            ->join('product_variants', 'product_variants.product_id', '=', 'products.id')
+            ->join('variant_attributes', 'variant_attributes.id', '=', 'product_variants.variant_attribute_id')
+            ->join('variant_attribute_values', 'variant_attribute_values.id', '=', 'product_variants.variant_attribute_value_id')
+            ->where('products.id', $id)
+            ->first();
+
+        $promoPrice = $product_details->promo_price ?? null;
 
         // Memeriksa kondisi dan menetapkan nilai promo_price menjadi NULL jika kondisi tidak terpenuhi
-        if ($product_details && $product_details['is_promo'] == 1 && $product_details['promo_start_date'] <= date('Y-m-d') && $product_details['promo_end_date'] >= date('Y-m-d')) {
+        if ($product_details && $product_details->is_promo == 1 && $product_details->promo_start_date <= date('Y-m-d') && $product_details->promo_end_date >= date('Y-m-d')) {
             // Kondisi promo berlaku, promo_price tetap seperti sebelumnya
-        
+
         } else {
             // Kondisi promo tidak berlaku, set promo_price menjadi NULL
             $promoPrice = null;
         }
 
-        $variants=VariantAttribute::with('attributeValue')->get();
+        $variants = VariantAttribute::with('attributeValue')->get();
 
         return response()->json([
-            
+
             "promoPrice" => $promoPrice,
             "product_details" => $product_details,
             // "variants" => $variants,
         ], 200);
     }
-
-    
-
 }
